1c5f161908d5c5df3c0f58ec829732142e3a47dd
Rotem <ro1478953@gmail.com>
2026-04-16 20:36:06 +0300

    Adding a function that adds items to the cart

---
+++ utils/quantity_parser.py
@@ -0,0 +1,13 @@
+import re
+
+def get_number_quantity(text: str) -> int | None:
+    clean_text = " ".join(text.split()).lower()
+
+    match = re.search(r"(\d+)\s*available", clean_text)
+    if match:
+        return int(match.group(1))
+
+    if clean_text.startswith("last one"):
+        return 1
+
+    return
